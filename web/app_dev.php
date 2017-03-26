3ecc194d5e3aac746aa57e3b24c6f52af6e8b0b8
catalin murasys <cata@mail.com>
2017-03-26 20:46:52 +0300

    cata_2 message

--- web/app_dev.php
+++ web/app_dev.php
@@ -21,6 +21,7 @@ if (isset($_SERVER['HTTP_CLIENT_IP'])
 /** @var \Composer\Autoload\ClassLoader $loader */
 $loader = require __DIR__.'/../app/autoload.php';
 Debug::enable();
+$dd = 22;
 
 $kernel = new AppKernel('dev', true);
 //$kernel->loadClassCache();
